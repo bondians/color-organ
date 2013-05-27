665d7b3e7edec73bfc2a38c6ac2985cc391ee816
James Cook <mokus@deepbondi.net>
2013-05-27 14:51:06 -0700

    messing with light output settings

--- Test.hs
+++ Test.hs
@@ -6,11 +6,11 @@ import qualified Data.Vector.Unboxed as U
 import Spectrum
 import VTree
 
-fftSize = 256
+fftSize = 2048
 sampleRate = 44100
 captureBufSize = 4096
 
-convert k = round . max 0 . min 255 . (k *) . subtract 1000
+convert k = round . max 0 . min 255 . subtract 20 . (k *)
 
 main = do
     s <- newSpectrum fftSize sampleRate
@@ -19,7 +19,7 @@ main = do
     
     forever $ do
         tickColorOrgan c s
-        set Red    v . convert 1e-1 =<< getMagnitude s 100
-        set Yellow v . convert 1e-1 =<< getMagnitude s 440
-        set Green  v . convert 1e-1 =<< getMagnitude s 1200
-        set Blue   v . convert 1e-3 =<< getMagnitude s 8000
+        set Red    v . convert 4e-4 =<< getMagnitude s 220
+        set Yellow v . convert 1e-4 =<< getMagnitude s 1200
+        set Green  v . convert 1e-3 =<< getMagnitude s 5000
+        set Blue   v . convert 3e-3 =<< getMagnitude s 16000
