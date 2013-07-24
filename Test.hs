627d8295438c29175d33dbb5b09251a5c063fe43
James Cook <mokus@deepbondi.net>
2013-07-23 22:38:56 -0700

    update test.hs for experimental xbee light
    along with some other unrelated changes...

--- Test.hs
+++ Test.hs
@@ -2,6 +2,7 @@
 module Main where
 
 import ColorOrgan
+import Control.Concurrent (threadDelay)
 import Control.Monad
 import Data.Complex
 import qualified Data.Vector.Unboxed as U
@@ -17,16 +18,16 @@ import Control.Concurrent.Thread.Group
 triangular j n = 1 - abs ((2 * j - n) / (n+1))
 hann j n = 0.5 * (1 - cos (2 * pi * (j+1) / (n+2)))
 
-fftSize = 2^9
+fftSize = 2^10
 window f = ST.fromList
     [ f (fromIntegral i) (fromIntegral fftSize - 1)
     | i <- [0 .. fftSize-1]
     ]
 sampleRate = 48000
-captureBufSize = 2^14
+captureBufSize = 2^12
 
-atk = 0.06
-dcy = 0.5
+atk = 0.09
+dcy = 0.6
 tuningPeriod = 120
 
 cutoff = 0.25
@@ -47,13 +48,12 @@ sample !n !wts !fs = ChanSample n (dot wts (U.map magSq fs))
 main = do
     s <- newSpectrum sampleRate fftSize (Just (window hann))
     c <- newColorOrgan sampleRate captureBufSize
-    v <- openVTreeLed "/dev/cu.usbmodem12341" 3
+    v <- openVTree "/dev/cu.usbmodem12341"
     
     freqs <- getBinFrequencies s
-    let [rWt, yWt, gWt, _, bWt] = map (tabulate freqs) xOver
+    let [rWt, gWt, _, _, bWt] = map (tabulate freqs) xOver
     
     rChan <- newChannel atk dcy (round (sampleRate * tuningPeriod))
-    yChan <- newChannel atk dcy (round (sampleRate * tuningPeriod))
     gChan <- newChannel atk dcy (round (sampleRate * tuningPeriod))
     bChan <- newChannel atk dcy (round (sampleRate * tuningPeriod))
     
@@ -68,12 +68,12 @@ main = do
             
             (_, b) <- forkIO tgrp2 $ feedChannel bChan (sample n bWt fs)
             (_, g) <- forkIO tgrp2 $ feedChannel gChan (sample n gWt fs)
-            (_, y) <- forkIO tgrp2 $ feedChannel yChan (sample n yWt fs)
             (_, r) <- forkIO tgrp2 $ feedChannel rChan (sample n rWt fs)
             
-            set Blue   v . ramp 80  =<< result =<< b
-            set Green  v . ramp 255 =<< result =<< g
-            set Yellow v . ramp 255 =<< result =<< y
-            set Red    v . ramp 255 =<< result =<< r
+            bVal <- fmap (ramp 65535) . result =<< b
+            gVal <- fmap (ramp 65535) . result =<< g
+            rVal <- fmap (ramp 65535) . result =<< r
+            
+            setRGB16 v (Addr16 2) rVal gVal bVal
             
             wait tgrp2
